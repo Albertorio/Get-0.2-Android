983e97dc6fcce8f5892967c2feb80e00c33e8dec
Luis <albertorio.10@hotmail.com>
2016-01-22 19:34:34 -0400

    sign up working now to set up profile

--- app/src/main/java/com/chamas/luis/getver2/MainActivity.java
+++ app/src/main/java/com/chamas/luis/getver2/MainActivity.java
@@ -1,5 +1,7 @@
 package com.chamas.luis.getver2;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +19,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
     LogInFragment logInFragment = new LogInFragment();
@@ -42,8 +51,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -77,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 
 
     /**
@@ -137,13 +146,66 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static class SignUpFragment extends Fragment{
+        private EditText Username, Email, password, ConfirmPass;
+        private Button SignupButton;
+        private String usernameStr, emailStr, passwordStr, confirmPassStr;
+
         public SignUpFragment (){
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-            return inflater.inflate(R.layout.fragment_sign_up, container, false);
+            View rootview = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+            Username = (EditText)rootview.findViewById(R.id.SignUpUsernameEditText);
+            Email = (EditText)rootview.findViewById(R.id.SignUpEmailEditText);
+            password = (EditText)rootview.findViewById(R.id.SignUpPasswordEditText);
+            ConfirmPass = (EditText)rootview.findViewById(R.id.SignUpConfirmPasswordEditText);
+            SignupButton = (Button)rootview.findViewById(R.id.SignUpButton);
+
+
+            SignupButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finishSignUp(v);
+                }
+            });
+
+
+            return rootview;
+        }
+
+        public void finishSignUp(View v){
+            usernameStr = Username.getText().toString();
+            emailStr = Email.getText().toString();
+            passwordStr = password.getText().toString();
+            // confirmPassStr = ConfirmPass.getText().toString();
+
+            ParseUser user = new ParseUser();
+            user.setUsername(usernameStr);
+            user.setPassword(passwordStr);
+            user.setEmail(emailStr);
+
+            final ProgressDialog progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("Signing up");
+            progressDialog.show();
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Toast.makeText(getActivity(), "sign up successful", Toast.LENGTH_LONG).show();
+                        Intent setupProfile = new Intent(getActivity(), SetUpProfile.class);
+                        getActivity().startActivity(setupProfile);
+                        getActivity().finish();
+                    } else {
+                        Toast.makeText(getActivity(), "failed", Toast.LENGTH_LONG).show();
+                        Log.d("parse error", e.toString());
+                        progressDialog.dismiss();
+                    }
+                }
+            });
         }
     }
 

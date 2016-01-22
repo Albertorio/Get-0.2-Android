d173127d035c953593e0bca614ea3e3aa96102aa
Luis <albertorio.10@hotmail.com>
2016-01-22 18:57:05 -0400

    redoing project with a new design

---
+++ app/src/main/java/com/chamas/luis/getver2/MainActivity.java
@@ -0,0 +1,162 @@
+package com.chamas.luis.getver2;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity {
+    LogInFragment logInFragment = new LogInFragment();
+    SignUpFragment signUpFragment = new SignUpFragment();
+    MainFragment mainFragment = new MainFragment();
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setCurrentItem(1);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+           if(position ==0){
+               return signUpFragment;
+           }else if(position == 1){
+               return mainFragment;
+           }else{
+               return logInFragment;
+           }
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class MainFragment extends Fragment {
+        public MainFragment(){
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+    }
+
+    public static class SignUpFragment extends Fragment{
+        public SignUpFragment (){
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        }
+    }
+
+    public static class LogInFragment extends Fragment{
+        public  LogInFragment(){
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+            return inflater.inflate(R.layout.fragment_log_in, container, false);
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/chamas/luis/getver2/Splash.java
@@ -0,0 +1,46 @@
+package com.chamas.luis.getver2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.parse.Parse;
+import com.parse.ParseUser;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "T4mKLSOGaa3dLavmKuvMlLvuXClmAdHxj295mOzc", "6KjuRBv415crv76Go4vqQyUD9Nt7pbCBS5CrLvQB");
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                ParseUser currentUser = ParseUser.getCurrentUser();
+//                if (currentUser != null) {
+//                    Intent eventIntent = new Intent(Splash.this, MainActivity.class);
+//                    Splash.this.startActivity(eventIntent);
+//                    Splash.this.finish();
+//                } else {
+//                    Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+//                    Splash.this.startActivity(mainIntent);
+//                    Splash.this.finish();
+//                }
+                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
+                Splash.this.startActivity(mainIntent);
+                Splash.this.finish();
+            }
+        }, 2000);
+    }
+
+}

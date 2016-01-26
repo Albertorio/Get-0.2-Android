160b6d7dc6090e2e73c9f721050465995262355b
Luis <albertorio.10@hotmail.com>
2016-01-26 09:12:27 -0400

    fixed picture, working on add event

---
+++ app/src/main/java/com/chamas/luis/getver2/AddEvent.java
@@ -0,0 +1,35 @@
+package com.chamas.luis.getver2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class AddEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/chamas/luis/getver2/EventList.java
+++ app/src/main/java/com/chamas/luis/getver2/EventList.java
@@ -1,9 +1,13 @@
 package com.chamas.luis.getver2;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -14,21 +18,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.SlidingDrawer;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
 
 import org.w3c.dom.Text;
 
+import java.io.File;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class EventList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     TextView navName, navEmail;
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private CharSequence mDrawerTitle;
-    private CharSequence mTitle;
-
+    ListAdapter listAdapter;
+    ListView eventList;
+    CircleImageView profileImage;
+    GoogleApiClient mGoogleApiClient;
 
 
     @Override
@@ -38,26 +54,68 @@ public class EventList extends AppCompatActivity implements NavigationView.OnNav
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        eventList = (ListView) findViewById(R.id.listView);
 
+
+
+        String[] values = new String[]{"Android List View",
+                "Adapter implementation",
+                "Simple List View In Android",
+                "Create List View Android",
+                "Android Example",
+                "List View Source Code",
+                "List View Array Adapter",
+                "Android Example List View"
+        };
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        // Assign adapter to ListView
+        eventList.setAdapter(adapter);
+
+        // ListView Item Click Listener
+        eventList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String itemValue = (String) eventList.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });
 
 
         ParseUser user = ParseUser.getCurrentUser();
-        final String name = (String)user.get("Name");
-        final String email = (String)user.get("email");
-
-//        Toast.makeText(this, "name: " + name, Toast.LENGTH_SHORT).show();
-//        Toast.makeText(this, "email: " + email, Toast.LENGTH_LONG).show();
+        final String name = (String) user.get("Name");
+        final String email = (String) user.get("email");
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                addEvent(view);
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
-
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -71,10 +129,40 @@ public class EventList extends AppCompatActivity implements NavigationView.OnNav
         View headerView = LayoutInflater.from(this).inflate(R.layout.nav_header_event_list, navigationView, false);
         navigationView.addHeaderView(headerView);
         navigationView.setNavigationItemSelectedListener(this);
-        navName = (TextView)headerView.findViewById(R.id.navname);
+
+        navName = (TextView) headerView.findViewById(R.id.navname);
         navName.setText(name);
         navEmail = (TextView) headerView.findViewById(R.id.navemail);
         navEmail.setText(email);
+
+        ParseFile fileObject = (ParseFile) user.get("Picture");
+        fileObject.getDataInBackground(new GetDataCallback() {
+            @Override
+            public void done(byte[] data, ParseException e) {
+                if (e == null) {
+                    //Decode the Byte[] into Bitmap
+                    Bitmap bpm = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    profileImage = (CircleImageView) findViewById(R.id.profile_image);
+                    profileImage.setImageBitmap(bpm);
+                } else {
+                    Log.d("Test", "Problem load image the data");
+                }
+            }
+        });
+
+    }
+
+    public void addEvent(View v) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == 1){
+            if(resultCode == Activity.RESULT_OK){
+
+            }
+        }
     }
 
     @Override
